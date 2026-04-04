6cb851a3c30e47ef3236b2349f58801e8304bed5
yeoreum <jeondaegam@naver.com>
2026-04-04 21:52:50 +0900

    출석체크 비즈니스로직 분리

--- src/main/java/com/example/attendanceevent/service/AttendanceService.java
+++ src/main/java/com/example/attendanceevent/service/AttendanceService.java
@@ -2,10 +2,10 @@ package com.example.attendanceevent.service;
 
 import com.example.attendanceevent.dto.Attendance;
 import com.example.attendanceevent.dto.PointHistory;
-import com.example.attendanceevent.dto.UserWallet;
 import com.example.attendanceevent.mapper.AttendanceMapper;
 import com.example.attendanceevent.mapper.PointMapper;
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,27 +28,38 @@ public class AttendanceService {
         }
 
         // 2. 실행: 출석 안했다면? 데이터 넣기
+        Attendance attendance = saveAttendance(userId);
+
+        // 3. 후속작업: 연속 일수 계산
+        int continuousDays = getContinuousDays(userId);
+
+        // 4. 보상
+        return processReward(userId, continuousDays, attendance.getId());
+    }
+
+    private String processReward(Long userId, int days, Long attendanceId) {
+        String message = "출석 완료! 현재 " + days + "일 연속 출석 중입니다.\n";
+
+        if (days == 7) {
+            int amount = 1000;
+            String reason = "7일 연속 출석";
+            rewardPoint(userId, amount, reason, attendanceId);
+            message += "\uD83D\uDE4C7일 연속 달성! 1,000 포인트 지급!\uD83D\uDE4C";
+        }
+        return message;
+    }
+
+    private int getContinuousDays(Long userId) {
+        return attendanceMapper.countContinuousAttendance(userId);
+    }
+
+
+    private @NonNull Attendance saveAttendance(Long userId) { //NonNull은 무슨의미지 여기서?
         Attendance attendance = new Attendance();
         attendance.setUserId(userId);
         attendanceMapper.insertAttendance(attendance);
+        return attendance;
         // insert 후에는 attendance.userId에 db가 생성한 번호가 자동으로 담겨 있다.
-        System.out.println("생성된 로그 ID: " + attendance.getId());
-
-        // 3. 후속작업: 연속 일수 계산 및 보상 메시지
-        int continuousDays = attendanceMapper.countContinuousAttendance(userId);
-        String message = "출석 완료! 현재 " + continuousDays + "일 연속 출석 중입니다.\n";
-
-        if (continuousDays == 7) {
-            message += "\uD83D\uDE4C7일 연속 달성! 1,000 포인트 지급!\uD83D\uDE4C";
-            // 여기에 포인트 지급 service.addPoint(userId, 1000) 같은 걸 넣으면 ok
-
-            int amount = +1000;
-            String reason = "7일 연속 출석";
-            rewardPoint(userId, amount, reason, attendance.getId());
-
-        }
-
-        return message;
     }
 
 
@@ -65,7 +76,6 @@ public class AttendanceService {
 //        UserWallet userWallet = new UserWallet(userId, amount);
 
     }
-
 
 
 }
